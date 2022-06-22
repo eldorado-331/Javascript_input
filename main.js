9baec1a8ac5c53a7dc884f70cd217964a30096d8
Ghali Ayomiposi <ayoghali@gmail.com>
2022-06-22 16:05:35 +0100

    First commit for JavaScript

---
+++ main.js
@@ -0,0 +1,5 @@
+var yourName, height, country
+yourName = prompt("Enter your Full Name")
+height = prompt("Enter your Height")
+country = prompt("Enter your Country")
+document.write(yourName + "<br>" + height + "<br>" + country)
